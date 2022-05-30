2f701596d5dbab96796d282b4fd4d7b948267623
JAEHWAN HWANG <jaehwan312@gmail.com>
2022-05-30 18:07:45 +0900

    jaehwan 5/30 18:07

---
+++ src/main/java/com/oracle/S20220601/S20220601Application.java
@@ -0,0 +1,13 @@
+package com.oracle.S20220601;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S20220601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S20220601Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/S20220601/S20220601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.S20220601;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S20220601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
