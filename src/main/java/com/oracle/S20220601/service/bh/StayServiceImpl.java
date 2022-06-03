745851ffb8906275e0be0d97f23bbc2b65d8ca40
ByeonhHeon Ha <qpflgood3@naver.com>
2022-06-03 17:45:03 +0900

    hbha 0603 5:44

--- src/main/java/com/oracle/S20220601/service/bh/StayServiceImpl.java
+++ src/main/java/com/oracle/S20220601/service/bh/StayServiceImpl.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.oracle.S20220601.dao.bh.StayDao;
 import com.oracle.S20220601.model.HostPhoto;
+import com.oracle.S20220601.model.RevPhoto;
 import com.oracle.S20220601.model.Review;
 import com.oracle.S20220601.model.Room;
 import com.oracle.S20220601.model.RoomPhoto;
+import com.oracle.S20220601.model.Stay;
 import com.oracle.S20220601.model.bh.HostStay;
 import com.oracle.S20220601.model.ih.HostStore;
 
@@ -46,6 +48,27 @@ public class StayServiceImpl implements StayService {
 		System.out.println("StayServiceImpl roomPhoto start...");
 		List<RoomPhoto> roomPhotoList = sd.roomPhotoList(roomPhoto);
 		return roomPhotoList;
+	}
+
+	@Override
+	public Stay stayinfo(int host_num) {
+		System.out.println("StayServiceImpl roomPhoto start...");
+		Stay stayinfo = sd.stayinfo(host_num);
+		return stayinfo;
+	}
+
+	@Override
+	public List<Review> reviewList(int host_num) {
+		System.out.println("StayServiceImpl reviewList start...");
+		List<Review> reviewList = sd.reviewList(host_num);
+		return reviewList;
+	}
+
+	@Override
+	public List<RevPhoto> reviewPhotoList(int host_num) {
+		System.out.println("StayServiceImpl reviewList start...");
+		List<RevPhoto> reviewPhotoList = sd.reviewPhotoList(host_num);
+		return reviewPhotoList;
 	}
 
 }
