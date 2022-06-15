3cc4969413fec9c50007356b01d23b203aca899a
inkim <inkim@DESKTOP-B02U1JG>
2022-06-15 09:09:51 +0900

    inkim 2022-06-16 09:09

--- src/main/resources/static/js/jj/mainPage.js
+++ src/main/resources/static/js/jj/mainPage.js
@@ -4,18 +4,29 @@
 // fullpage 준비
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-				onLeave: function(origin, destination, direction){
-					var leavingSection = this;
-
+				'afterLoad': function (anchorLink, index) {
+					if (index == 2){
+						$('.counter').show();
+						$('.substitute').hide();
+						$('.counter').counterUp({
+						    delay: 10,
+						    time: 2000
+						});	
+					}
+				},	
+		
+				// 페이지 이동할 때
+				'onLeave' : function (index, nextIndex, direction){
+					if (index == 2 && direction == 'down'){
+						$('.counter').attr('class','counte');
+					} else if (index == 2 && direction == 'up'){
+						$('.counter').attr('class','counte');
+					}
 				}
 			});
 		});
 
-// 메인화면 숫자 카운팅효과
- $('.counter').counterUp({
-    delay: 10,
-    time: 2000
-});		
+
 
 // 메인페이지에서 검색바 헤더에서 숨기기
 $(function(){
@@ -230,12 +241,22 @@ function mainEnter(){
 	}
 }
 
+// 검색 유효성 검사(메인)
+function checkIt(){
+		var searchBarValue = $('#searchBar').val();
+
+		if (searchBarValue.replace(/\s|　/gi, '')==""){	  // 자바스크립트 : 빈문자열 -> false 반환
+			$('#searchBar').val("");
+			$('#searchBar').focus();
+			return false;
+		}
+	}
+
 //검색바에 입력한 값으로 검색 실행
 function inputSearch(){
 	
-	var text = $('#searchBar').val();
-	var empty = /\s/g; 
-	if(text.replace(empty, "")==""){
+	var text = document.getElementById("searchBar").value; 
+	if(text.replace(/\s|　/gi, '')==""){
 		alert('검색어를 입력해주세요');
 		$('#searchBar').val("");
 		$('#searchBar').focus();
@@ -252,4 +273,3 @@ function selectSearch_main(id){
 	$('#searchBar').val(selectedKeyword);
 	document.getElementById('main_search_area').submit();
 }
-
